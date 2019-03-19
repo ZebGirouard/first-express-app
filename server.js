4286b9e7ef68f12a8224a7095444c96af2236bf7
ZebGirouard <zeb.girouard@generalassemb.ly>
2019-03-18 19:58:16 -0600

    More mongo

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser')
 const app     = express();
 const port    = process.env.PORT || 3000;
+const url = 'mongodb://localhost:27017';
+
+app.use(bodyParser.json())
 
 const taquerias = [
   {name: 'La Taqueria'},
@@ -22,7 +27,40 @@ function homeController(req, res) {
 app.get('/', homeController);
 
 app.get('/api/taquerias', (req, res) => {
-  res.json(taquerias)
+  MongoClient.connect(url, (err, client) => {
+    // make sure we connected
+    if (!err) {
+      console.log("Connected successfully to server");
+    }
+    //connect to the taqueria collection in the myproject db
+    const db = client.db('myproject');
+    const taquerias = db.collection('taquerias');
+
+    //add the taqueria
+    taquerias.find({}).toArray((err, docs) => {
+      console.log(docs);
+      client.close();
+      // Send all taquerias back to the front end
+      res.json(docs);
+    })
+  });
+});
+
+app.post('/api/taquerias', (req, res) => {
+  console.log(req.body)
+  MongoClient.connect(url, (err, client) => {
+    if (!err) {
+      console.log("Connected successfully to server");
+    }
+    const db = client.db('myproject')
+    const taquerias = db.collection('taquerias')
+
+    taquerias.insertOne(req.body, (err, result) => {
+      console.log(result)
+      client.close()
+      res.send('ok')
+    })
+  })
 })
 
 // start server
